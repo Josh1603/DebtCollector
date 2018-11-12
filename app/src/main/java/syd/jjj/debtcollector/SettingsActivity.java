2f0c49d2e3fad3f2c22f5f21d4fdfc78e6af6ba9
Josh Hardman <joshdmbh@gmail.com>
2018-11-12 17:21:39 +1100

    Main alteration: Introduced theme settings and provided 3 preset themes to select from the Settings menu.
    Other alterations: Settings icon changed to more widely recognised cog icon. Overflow icon replaced with action bar icon.

--- app/src/main/java/syd/jjj/debtcollector/SettingsActivity.java
+++ app/src/main/java/syd/jjj/debtcollector/SettingsActivity.java
@@ -1,16 +1,50 @@
 package syd.jjj.debtcollector;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences sharedPreferences;
+    private String currentTheme;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        currentTheme = sharedPreferences.getString("theme_settings_list", "defaultTheme");
+
+        if (currentTheme.equals("defaultTheme")) {
+            setTheme(R.style.DefaultAppTheme);
+        }
+
+        if (currentTheme.equals("halloweenTheme")) {
+            setTheme(R.style.Halloween);
+        }
+
+        if(currentTheme.equals("rosesTheme")){
+            setTheme(R.style.RosesTheme);
+        }
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+
         super.onCreate(savedInstanceState);
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-
     }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if(key.equals("theme_settings_list")) {
+                    Intent intent = new Intent(this, SettingsActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+    }
+
 }
